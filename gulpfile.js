122b4ea8ff76409e6384e6fbba0854d48c9e7190
KISHI Yoshimaru <kishi@beeworks.co.jp>
2020-08-06 21:43:55 +0900

    [add] scssコンパイル追加

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,18 @@
 
 const gulp = require('gulp');
 const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
+const postcss = require('gulp-postcss');
+const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync').create();
 
 const path = {
   srcDir: './_src',
   src: {
     html: 'pug',
-    css: 'sass',
+    css: 'scss',
     js: 'js',
     image: 'image',
   },
@@ -30,6 +35,17 @@ function compilePug() {
     .pipe(gulp.dest(`${path.destDir}/${path.dest.html}`));
 }
 
+// CSS
+function compileScss() {
+  return gulp
+    .src(`${path.srcDir}/${path.src.css}/*.scss`)
+    .pipe(plumber())
+    .pipe(sassGlob())
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(gulp.dest(`${path.destDir}/${path.dest.css}`));
+}
+
 // ホットリロード
 function sync() {
   browserSync.init({
@@ -42,10 +58,14 @@ function reload(done) {
 }
 function watch() {
   gulp.watch(
-    `${path.srcDir}/${path.src.html}/*.html`,
+    `${path.srcDir}/${path.src.html}/*`,
     gulp.series(compilePug, reload)
+  );
+  gulp.watch(
+    `${path.srcDir}/${path.src.css}/*`,
+    gulp.series(compileScss, reload)
   );
 }
 
-exports.build = gulp.parallel(compilePug);
+exports.build = gulp.parallel(compilePug, compileScss);
 exports.watch = gulp.parallel(sync, watch);
