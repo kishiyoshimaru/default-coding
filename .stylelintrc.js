7013b7beee5d4ba337afc45de0ec066f31ffa34b
KISHI Yoshimaru <kishi@beeworks.co.jp>
2020-08-12 20:10:11 +0900

    [add] stylelintにstylelint-config-recess-orderを追加

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: [
-    "stylelint-config-recommended-scss",
-    "stylelint-prettier/recommended"
+    'stylelint-config-recess-order',
+    'stylelint-config-recommended-scss',
+    'stylelint-prettier/recommended'
   ],
 };
